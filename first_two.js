f92bd1e9307cad0be549509e9297ea57e17c0636
pratyush197 <anil.pratyushsingh@gmail.com>
2018-02-02 16:04:04 +0530

    message

---
+++ first_two.js
@@ -0,0 +1,11 @@
+var http = require("http");  
+http.createServer(function (request, response) {  
+ // Send the HTTP header   
+   // HTTP Status: 200 : OK  
+   // Content Type: text/plain  
+   response.writeHead(200, {'Content-Type': 'text/plain'});  
+   // Send the response body as "Hello World"  
+   response.end('My test program of CI and CD \n');  
+}).listen(8081);  
+// Console will print the message  
+console.log('Server running fine'); 
